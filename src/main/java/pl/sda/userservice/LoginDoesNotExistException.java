e57a09760996c686e965daa9fd41a753c997a22e
Marcin Iskra <dunravel@gmail.com>
2018-10-26 23:23:44 +0200

    UserService:LoginDoesNotExistException - Implement: exception contains correct message with given login

--- src/main/java/pl/sda/userservice/LoginDoesNotExistException.java
+++ src/main/java/pl/sda/userservice/LoginDoesNotExistException.java
@@ -2,5 +2,6 @@ package pl.sda.userservice;
 
 class LoginDoesNotExistException extends RuntimeException {
     LoginDoesNotExistException(String login) {
+        super("Login does not exist: " + login);
     }
 }
